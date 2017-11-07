bdb4e9bfd6eb93c94ba185621e672377b9a75be0
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-07 15:08:24 -0800

    Add problem boilerplate

--- Data Structures/Arrays - Left Rotation/Arrays - Left Rotation/main.cpp
+++ Data Structures/Arrays - Left Rotation/Arrays - Left Rotation/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
     return 0;
 }
