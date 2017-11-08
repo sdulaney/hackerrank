732b00b6fcadd0eeeaa050116b55e14627091d71
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-07 16:06:11 -0800

    Add problem boilerplate

--- Data Structures/Strings - Making Anagrams/Strings - Making Anagrams/main.cpp
+++ Data Structures/Strings - Making Anagrams/Strings - Making Anagrams/main.cpp
@@ -6,10 +6,40 @@
 //  Copyright © 2017 Stewart Dulaney. All rights reserved.
 //
 
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int number_needed(string a, string b) {
+    
+}
+
+int main(){
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << number_needed(a, b) << endl;
     return 0;
 }
