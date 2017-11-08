6ea5164d55329d44ecacc3346120bf2f2130c95a
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-07 19:03:13 -0800

    Add problem boilerplate

--- Data Structures/Linked Lists - Detect a Cycle/Linked Lists - Detect a Cycle/main.cpp
+++ Data Structures/Linked Lists - Detect a Cycle/Linked Lists - Detect a Cycle/main.cpp
@@ -8,6 +8,26 @@
 
 #include <iostream>
 
+/*
+ Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+ 
+ A Node is defined as:
+ struct Node {
+ int data;
+ struct Node* next;
+ }
+ */
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
