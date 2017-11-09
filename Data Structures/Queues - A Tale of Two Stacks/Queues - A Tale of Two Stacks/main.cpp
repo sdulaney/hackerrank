bb8aee1359b19e13f30a77f67349b35894bff002
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-08 17:42:49 -0800

    Add problem boilerplate

--- Data Structures/Queues - A Tale of Two Stacks/Queues - A Tale of Two Stacks/main.cpp
+++ Data Structures/Queues - A Tale of Two Stacks/Queues - A Tale of Two Stacks/main.cpp
@@ -6,10 +6,49 @@
 //  Copyright © 2017 Stewart Dulaney. All rights reserved.
 //
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class MyQueue {
+    
+public:
+    stack<int> stack_newest_on_top, stack_oldest_on_top;
+    void push(int x) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int front() {
+        
+    }
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
 }
+
