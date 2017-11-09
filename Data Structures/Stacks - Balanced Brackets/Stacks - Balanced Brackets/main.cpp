0e4e8c858093a9b1ae3eb354a44ce88531b2d430
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-08 16:47:31 -0800

    Add code for is_balanced

--- Data Structures/Stacks - Balanced Brackets/Stacks - Balanced Brackets/main.cpp
+++ Data Structures/Stacks - Balanced Brackets/Stacks - Balanced Brackets/main.cpp
@@ -6,33 +6,28 @@
 //  Copyright © 2017 Stewart Dulaney. All rights reserved.
 //
 
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <string>
+#include <stack>
 
 using namespace std;
 
 bool is_balanced(string expression) {
-    
+    stack<char> s;
+    for(int i=0; i<expression.length(); i++){
+        if(expression[i] == '{')
+            s.push('}');
+        else if(expression[i] == '[')
+            s.push(']');
+        else if(expression[i] == '(')
+            s.push(')');
+        else {
+            if(s.empty() || expression[i] != s.top())
+                return false;
+            s.pop();
+        }
+    }
+    return s.empty();
 }
 
 int main(){
