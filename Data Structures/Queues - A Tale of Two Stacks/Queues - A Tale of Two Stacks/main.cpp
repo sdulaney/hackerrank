6acd75f890fa0233c494ce16ac13968b0bde18e6
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-08 17:42:25 -0800

    Setup

---
+++ Data Structures/Queues - A Tale of Two Stacks/Queues - A Tale of Two Stacks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queues - A Tale of Two Stacks
+//
+//  Created by Stewart Dulaney on 11/8/17.
+//  Copyright © 2017 Stewart Dulaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
