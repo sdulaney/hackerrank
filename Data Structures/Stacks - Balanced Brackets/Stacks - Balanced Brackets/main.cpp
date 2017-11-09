f3c3069f7df3001e2e50c982909bc71b0b3fd5a7
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-08 16:17:35 -0800

    Add problem boilerplate

--- Data Structures/Stacks - Balanced Brackets/Stacks - Balanced Brackets/main.cpp
+++ Data Structures/Stacks - Balanced Brackets/Stacks - Balanced Brackets/main.cpp
@@ -6,10 +6,46 @@
 //  Copyright © 2017 Stewart Dulaney. All rights reserved.
 //
 
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool is_balanced(string expression) {
+    
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        string expression;
+        cin >> expression;
+        bool answer = is_balanced(expression);
+        if(answer)
+            cout << "YES\n";
+        else cout << "NO\n";
+    }
     return 0;
 }
+
