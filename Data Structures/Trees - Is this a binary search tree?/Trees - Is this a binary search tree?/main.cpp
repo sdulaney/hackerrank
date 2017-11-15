4ee76fc97e66ee4101904f2c48804c7f2fc50851
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-14 16:49:35 -0800

    Add code for checkBST and isBST

--- Data Structures/Trees - Is this a binary search tree?/Trees - Is this a binary search tree?/main.cpp
+++ Data Structures/Trees - Is this a binary search tree?/Trees - Is this a binary search tree?/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <climits>
 
 /* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
  
@@ -24,8 +25,16 @@ struct Node {
     Node* right;
 };
 
+bool isBST(Node* node, int min, int max) {
+    if(node == NULL)
+        return true;
+    if(node->data < min || node->data > max)
+        return false;
+    return isBST(node->left, min, node->data-1) && isBST(node->right, node->data+1, max);
+}
+
 bool checkBST(Node* root) {
-    
+    return isBST(root, INT_MIN, INT_MAX);
 }
 
 int main(int argc, const char * argv[]) {
