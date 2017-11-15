525d1ea959bfc7fff0b9fb1a6894cddfd5962654
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-15 00:46:54 -0800

    Add code for addNumber, rebalance, getMedian

--- Data Structures/Heaps - Find the Running Median/Heaps - Find the Running Median/main.cpp
+++ Data Structures/Heaps - Find the Running Median/Heaps - Find the Running Median/main.cpp
@@ -9,20 +9,58 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <iomanip>
 
 using namespace std;
+
+void addNumber(int num, priority_queue<int, vector<int>, less<int> >* lowers, priority_queue<int, vector<int>, greater<int> >* highers){
+    if(lowers->size() ==  0 || num < lowers->top())
+        lowers->push(num);
+    else
+        highers->push(num);
+}
+
+void rebalance(priority_queue<int, vector<int>, less<int> >* lowers, priority_queue<int, vector<int>, greater<int> >* highers){
+    if(lowers->size() > highers->size()){
+        if(lowers->size() - highers->size() > 1){
+            highers->push(lowers->top());
+            lowers->pop();
+        }
+    }
+    else {
+        if(highers->size() - lowers->size() > 1){
+            lowers->push(highers->top());
+            highers->pop();
+        }
+    }
+}
+
+double getMedian(priority_queue<int, vector<int>, less<int> >* lowers, priority_queue<int, vector<int>, greater<int> >* highers){
+    if(lowers->size() == highers->size()){
+        return 0.5 * (lowers->top() + highers->top());
+    }
+    else {
+        if(lowers->size() > highers->size()){
+                return 1.0 * lowers->top();
+        }
+        else if(highers->size() > lowers->size()){
+                return 1.0 * highers->top();
+        }
+    }
+    return 0.0;
+}
 
 int main(){
     int n;
     cin >> n;
     vector<int> a(n);
-    priority_queue<int> pqMax;
+    priority_queue<int, vector<int>, less<int> > pqMax;
     priority_queue<int, vector<int>, greater<int> > pqMin;
     for(int a_i = 0;a_i < n;a_i++){
         cin >> a[a_i];
-        addNumber(a[a_i], pqMax, pqMin);
-        rebalance(pqMax, pqMin);
-        cout << getMedian(pqMax, pqMin) << endl;
+        addNumber(a[a_i], &pqMax, &pqMin);
+        rebalance(&pqMax, &pqMin);
+        cout << fixed << setprecision(1) << getMedian(&pqMax, &pqMin) << endl;
     }
     return 0;
 }
