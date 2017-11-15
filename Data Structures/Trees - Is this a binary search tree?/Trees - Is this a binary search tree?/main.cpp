1b7f70697ed91ecf8c5aa6cb638549219162a2d5
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-14 16:42:00 -0800

    Add problem boilerplate

--- Data Structures/Trees - Is this a binary search tree?/Trees - Is this a binary search tree?/main.cpp
+++ Data Structures/Trees - Is this a binary search tree?/Trees - Is this a binary search tree?/main.cpp
@@ -8,6 +8,26 @@
 
 #include <iostream>
 
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
+ 
+ The Node struct is defined as follows:
+ struct Node {
+ int data;
+ Node* left;
+ Node* right;
+ }
+ */
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool checkBST(Node* root) {
+    
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
