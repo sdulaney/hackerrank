cede07074e2f2f6704837f0de5ecaad4de31cde6
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-14 18:46:34 -0800

    Modularize code

--- Data Structures/Heaps - Find the Running Median/Heaps - Find the Running Median/main.cpp
+++ Data Structures/Heaps - Find the Running Median/Heaps - Find the Running Median/main.cpp
@@ -8,16 +8,21 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
-
 
 int main(){
     int n;
     cin >> n;
     vector<int> a(n);
+    priority_queue<int> pqMax;
+    priority_queue<int, vector<int>, greater<int> > pqMin;
     for(int a_i = 0;a_i < n;a_i++){
         cin >> a[a_i];
+        addNumber(a[a_i], pqMax, pqMin);
+        rebalance(pqMax, pqMin);
+        cout << getMedian(pqMax, pqMin) << endl;
     }
     return 0;
 }
