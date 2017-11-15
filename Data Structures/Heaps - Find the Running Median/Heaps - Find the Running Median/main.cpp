66c449f95904048d8d1147f179bdd42f1ca221c6
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-14 16:55:11 -0800

    Setup

---
+++ Data Structures/Heaps - Find the Running Median/Heaps - Find the Running Median/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heaps - Find the Running Median
+//
+//  Created by Stewart Dulaney on 11/14/17.
+//  Copyright © 2017 Stewart Dulaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
