2070d8cff6617aa5cd8f55b853855e834fe60c16
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-11-15 17:12:00 -0800

    Setup

---
+++ Data Structures/Tries - Contacts/Tries - Contacts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tries - Contacts
+//
+//  Created by Stewart Dulaney on 11/15/17.
+//  Copyright © 2017 Stewart Dulaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
