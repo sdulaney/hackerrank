fac4bca72ced504ee77ceb240e66c2895db4f930
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-12-18 12:48:01 -0800

    Add problem boilerplate

--- Algorithms/Sorting - Comparator/Sorting - Comparator/main.cpp
+++ Algorithms/Sorting - Comparator/Sorting - Comparator/main.cpp
@@ -6,10 +6,40 @@
 //  Copyright © 2017 Stewart Dulaney. All rights reserved.
 //
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+struct Player {
+    string name;
+    int score;
+};
+
+vector<Player> comparator(vector<Player> players) {
+    
+}
+
+int main() {
+    
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player p1;
+        p1.name = name, p1.score = score;
+        players.push_back(p1);
+    }
+    
+    vector<Player > answer = comparator(players);
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i].name << " " << answer[i].score << endl;
+    }
     return 0;
 }
