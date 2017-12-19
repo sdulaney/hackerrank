29c3a3d5a34c7ba986ca70cc9bf45a293a54dfb3
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-12-19 10:58:56 -0800

    Setup

---
+++ Algorithms/Merge Sort - Counting Inversions/Merge Sort - Counting Inversions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Merge Sort - Counting Inversions
+//
+//  Created by Stewart Dulaney on 12/19/17.
+//  Copyright © 2017 Stewart Dulaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
