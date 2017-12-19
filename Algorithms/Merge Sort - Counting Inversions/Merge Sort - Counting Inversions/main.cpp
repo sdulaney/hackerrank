b9de11695da67dfa69d339f3645a239889bef171
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-12-19 11:04:12 -0800

    Add problem boilerplate

--- Algorithms/Merge Sort - Counting Inversions/Merge Sort - Counting Inversions/main.cpp
+++ Algorithms/Merge Sort - Counting Inversions/Merge Sort - Counting Inversions/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+long countInversions(int arr) {
+    // Complete this function
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            cin >> arr[arr_i];
+        }
+        long result = countInversions(arr);
+        cout << result << endl;
+    }
     return 0;
 }
