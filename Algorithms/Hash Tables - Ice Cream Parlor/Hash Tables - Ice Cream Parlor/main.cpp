0efca34e418847bb85cd2dde8a425835efe3db8b
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-12-30 12:02:55 -0800

    add problem boilerplate

--- Algorithms/Hash Tables - Ice Cream Parlor/Hash Tables - Ice Cream Parlor/main.cpp
+++ Algorithms/Hash Tables - Ice Cream Parlor/Hash Tables - Ice Cream Parlor/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void solve(vector <int> arr, int money) {
+    // Complete this function
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int money;
+        cin >> money;
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            cin >> arr[arr_i];
+        }
+        solve(arr, money);
+    }
     return 0;
 }
