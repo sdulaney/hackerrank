9271f5c6ebdb59df205e329637ed45039c0426ef
Stewart Dulaney <sdulaney@users.noreply.github.com>
2017-12-30 12:01:03 -0800

    set-up problem

---
+++ Algorithms/Hash Tables - Ice Cream Parlor/Hash Tables - Ice Cream Parlor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hash Tables - Ice Cream Parlor
+//
+//  Created by Stewart Dulaney on 12/30/17.
+//  Copyright © 2017 Stewart Dulaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
